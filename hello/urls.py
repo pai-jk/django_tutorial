12ec506a004e859d70ad21fb222a8449e37c056e
pai_jk <pai_jk@pai-jk.local>
2023-02-17 10:36:16 +0900

    edit 추가

--- hello/urls.py
+++ hello/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('post_list', views.post_list, name='post_list'),
     path('post_detail/<int:pk>', views.post_detail, name="post_detail"),
     path('post_new', views.post_new, name="post_new"),
+    path('post_detail/<int:pk>/edit', views.post_edit, name="post_edit"),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from .forms import PostForm
 
@@ -33,3 +33,21 @@ def post_new(request):
         raise ValueError
 
     return render(request, 'hello/post_new.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('hello:post_detail', pk=post.pk)
+        pass
+    elif request.method == "GET":
+        form = PostForm(instance=post)
+        # form = Post.objects.get(pk=pk)
+
+    else:
+        raise ValueError
+    return render(request, 'hello/post_edit.html', {'form': form})
